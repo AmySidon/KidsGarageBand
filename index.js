d870de3422bcb260cae2c8e78bec03c12e779414
AmySidon <108723547+AmySidon@users.noreply.github.com>
2024-06-23 02:15:54 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+//Detecting button press
+var allButtons = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i < allButtons; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        var buttonInnerHTML = this.innerHTML
+        this.style.color = "yellow";
+        makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
+        
+    });
+}
+
+
+//Detecting keyboard press
+document.addEventListener("keypress", function(event){
+    makeSound(event.key);
+
+    buttonAnimation(event.key);
+
+//    console.log(event);
+//alert("key was pressed");
+});  
+
+function makeSound(key){
+    switch(key){
+        case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+
+        case "a":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+
+        case "s":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+
+        case "d":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+
+        case "j":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+
+        case "k":
+        var crash = new Audio("sounds/crash.mp3")
+        crash.play();
+        break;
+        case "l":
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+        
+        default: console.log(buttonInnerHTML);
+    }
+}
+
+//Change the style of the buttons!  
+function buttonAnimation(currenteKey){
+
+   var activeButton = document.querySelector("." + currenteKey);
+
+   activeButton.classList.add("pressed"); //Using the CSS class called .pressed to style button
+   
+   setTimeout(function(){
+    activeButton.classList.remove("pressed");
+
+   }, 200);
+}
